68ed49a503f04ffcd914d46db3c9adf4d2cd865d
jonastschumi <jonas@tschumi.io>
2023-09-26 09:09:12 +0200

    added project

--- app/drone/page.tsx
+++ app/drone/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import React from "react";
+import Footer from "@/app/components/footer";
 
 export default function Page() {
     return (
-        <main className="white flex min-h-screen flex-col items-center justify-between p-24">
-            <h2>Drone</h2>
-        </main>
+        <div>
+            <h2>Equipment</h2>
+            <Footer/>
+        </div>
     )
 }
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/app/components/footer";
 export default function Page() {
     return (
         <div>
-            <h2>About</h2>
+            <h2>Project</h2>
             <Footer/>
         </div>
     )
