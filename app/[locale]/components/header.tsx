9fede93f01abbaf8dfc6222809de3f793f3eb10b
jonastschumi <jonas@tschumi.io>
2023-09-30 11:50:17 +0200

    typo

--- app/[locale]/components/header.tsx
+++ app/[locale]/components/header.tsx
@@ -102,7 +102,7 @@ export default function Header() {
                                     About
                                 </Link>
                                 <Link
-                                    href="/projects"
+                                    href="/project"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-foreground hover:bg-gray-500"
                                 >
                                     Projects
