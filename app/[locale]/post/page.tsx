03bbfed1c88e2845547dd3f87a2c61e140283ad7
jonastschumi <jonas@tschumi.io>
2023-09-30 16:11:14 +0200

    first draft posts

--- app/[locale]/post/page.tsx
+++ app/[locale]/post/page.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import Header from "@/app/[locale]/components/header";
 import Section from "@/app/[locale]/components/section";
 import SectionLeft from "@/app/[locale]/components/sectionLeft";
+import path from "path";
+import * as fs from "fs";
 
 export default function Page() {
+    // read all files from public/posts and get the paths
+    const paths = fs.readdirSync(path.join(process.cwd(), "public/posts"));
     return (
         <div>
             <Header/>
-            <Section image="/portrait.png" title="Titel1" text="lorem ipsum" dark={true}/>
-            <SectionLeft image="/portrait.png" title="Titel1" text="lorem ipsum" dark={false}/>
-            <Section image="/portrait.png" title="Titel1" text="lorem ipsum" dark={true}/>
+            {paths.map((path) => `/posts/${path}`).map((path) => {
+                return (
+                    <div className="relative">
+                        <video autoPlay loop muted className="w-full object-cover">
+                            <source src={path} type="video/mp4"/>
+                        </video>
+                    </div>
+                )
+            })}
         </div>
     )
 }
