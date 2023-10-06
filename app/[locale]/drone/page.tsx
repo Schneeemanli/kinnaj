f99317ffc6bfeeef80b33b314474c1d4c6068bf1
Jannik Hug <jannik.hug@gmx.ch>
2023-10-06 17:25:50 +0200

    started with equip

--- app/[locale]/drone/page.tsx
+++ app/[locale]/drone/page.tsx
@@ -7,13 +7,17 @@ import SectionLeft from "@/app/[locale]/components/sectionLeft";
 import {useTranslations} from "next-intl";
 
 export default function Page() {
-    const t = useTranslations('Project');
+    const t = useTranslations('Equipment');
     return (
         <div>
             <Header/>
-            <Section image="/portrait.png" title="Titel1" text="lorem ipsum" dark={true}/>
-            <SectionLeft image="/portrait.png" title="Titel1" text="lorem ipsum" dark={false}/>
-            <Section image="/portrait.png" title="Titel1" text="lorem ipsum" dark={true}/>
+            <Section image="/equipment/custom5.jpg" title={t("1t")} text={t("1d")} dark={true}/>
+            <SectionLeft image="/equipment/customO3.jpg" title={t("2t")} text={t("2t")} dark={false}/>
+            <Section image="/equipment/cine.jpg" title={t("3t")} text={t("3t")} dark={true}/>
+            <SectionLeft image="/equipment/avata.jpg" title={t("4t")} text={t("4t")} dark={false}/>
+            <Section image="/equipment/googles2.jpg" title={t("5t")} text={t("5t")} dark={true}/>
+            <SectionLeft image="/equipment/googlesV2.jpg" title={t("6t")} text={t("6t")} dark={false}/>
+            <Section image="/equipment/portrait.png" title={t("7t")} text={t("7t")} dark={true}/>
         </div>
     )
 }
