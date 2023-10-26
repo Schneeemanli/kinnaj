b2667b76f6f5e97e2ea79aa2757d5f2e89869576
Jannik <jannik.hug@gmx.ch>
2023-10-26 10:14:20 +0200

    Added contect

--- app/[locale]/project/page.tsx
+++ app/[locale]/project/page.tsx
@@ -5,14 +5,31 @@ import Section from "@/app/[locale]/components/section";
 import SectionLeft from "@/app/[locale]/components/sectionLeft";
 import SectionVideo from "@/app/[locale]/components/sectionVideo";
 import SectionLeftVideo from "@/app/[locale]/components/sectionLeftVideo";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
-    return (
-        <div>
-            <Header/>
-            <SectionVideo video="/titleVideo.mp4" title="Titel1" text={["lorem ipsum"]} dark={true}/>
-            <SectionLeftVideo video="/titleVideo.mp4" title="Titel1" text={["lorem ipsum"]} dark={false}/>
-            <SectionVideo video="/titleVideo.mp4" title="Titel1" text={["lorem ipsum"]} dark={true}/>
-        </div>
-    )
+  const t = useTranslations("Projects");
+  return (
+    <div>
+      <Header />
+      <SectionVideo
+        video="/titleVideo.mp4"
+        title={t("bastellaTitle")}
+        text={[t("bastellaDescription")]}
+        dark={true}
+      />
+      <SectionLeftVideo
+        video="/titleVideo.mp4"
+        title="Titel1"
+        text={["lorem ipsum"]}
+        dark={false}
+      />
+      <SectionVideo
+        video="/titleVideo.mp4"
+        title="Titel1"
+        text={["lorem ipsum"]}
+        dark={true}
+      />
+    </div>
+  );
 }
