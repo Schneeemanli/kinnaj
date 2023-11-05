2e45737ab4154040541726b52b5886eadb1f1ede
jonastschumi <jonas@tschumi.io>
2023-11-05 13:35:27 +0100

    changed fpv

--- app/[locale]/components/fpv.tsx
+++ app/[locale]/components/fpv.tsx
@@ -6,31 +6,50 @@ import {useTranslations} from "next-intl";
 export default function Fpv() {
     const t = useTranslations("Home");
     return (
-        <div className="bg-primary-50 p-5 md:p-8 lg:p-11 relative">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <RevealOnScroll axis="x">
-                    <div className="p-4 bg-white shadow-lg rounded-3xl animate-move-left">
-                        <h2 className="text-2xl text-gray-700 font-semibold mb-4">{t("fpvTitel")}</h2>
-                        <p className="text-gray-700">{t("whatIsFpv")}</p>
-                        <p className="text-gray-700 mt-4">{t("offersFPV")}</p>
+        <div className="relative flex flex-wrap">
+            <div className="w-full sm:w-1/2">
+                <div className="bg-primary-400 p-6">
+                    <h2 className="text-2xl font-semibold mb-4">{t("fpvTitel")}</h2>
+                    <p>{t("whatIsFpv")}</p>
+                    <p className="mt-4">{t("offersFPV")}</p>
+                    <div className="flex items-center justify-center pt-5">
+                        <Link
+                            href="/drone"
+                            className="bg-primary-200 hover:bg-primary-300 text-white font-semibold py-2 px-4 rounded-full"
+                        >
+                            {t("buttonEquipment")}
+                        </Link>
                     </div>
-                </RevealOnScroll>
-                <RevealOnScroll axis="x">
-                    <div className="p-4 bg-white shadow-lg rounded-3xl animate-move-right">
-                        <h2 className="text-2xl text-gray-700 font-semibold mb-4">Ausrüstung</h2>
-                        <ul className="list-disc list-inside text-gray-700">
-                            <li>FPV-Brille oder -Monitor</li>
-                            <li>Drohne oder Flugzeug mit FPV-Kamera</li>
-                            <li>Sender und Empfänger</li>
-                            <li>Steuerungskonsole</li>
-                        </ul>
-                    </div>
-                </RevealOnScroll>
+                </div>
             </div>
-            <div className="flex items-center justify-center pt-5">
-                <Link href="/drone" className="bg-primary-200 hover:bg-primary-300 text-white font-semibold py-2 px-4 rounded-full">
-                    {t("buttonEquipment")}
-                </Link>
+            <div className="w-full sm:w-1/2 bg-primary-50 flex items-center justify-center">
+                <div className="text-white p-6">
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="mb-8">
+                            <img
+                                src="/certs/MicrosoftCertifiedAssociate.png"
+                                alt="Bild 1"
+                                className="h-40 object-contain rounded-md"
+                            />
+                        </div>
+                        <div className="flex">
+                            <div className="mr-8">
+                                <img
+                                    src="/certs/MicrosoftCertifiedAssociate.png"
+                                    alt="Bild 2"
+                                    className="h-40 object-contain rounded-md"
+                                />
+                            </div>
+                            <div>
+                                <img
+                                    src="/certs/MicrosoftCertifiedAssociate.png"
+                                    alt="Bild 3"
+                                    className="h-40 object-contain rounded-md"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
