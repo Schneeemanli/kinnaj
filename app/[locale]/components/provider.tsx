21c89e8041980af2263e859aee9fdcf2849a33ec
jonastschumi <jonas@tschumi.io>
2023-12-07 20:21:16 +0100

    bug fix

--- app/[locale]/components/provider.tsx
+++ app/[locale]/components/provider.tsx
@@ -6,7 +6,7 @@ import {ThemeProvider as NextThemesProvider} from "next-themes";
 export function Providers({children}: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            <NextThemesProvider defaultTheme="white">
+            <NextThemesProvider defaultTheme="custom-white">
                 {children}
             </NextThemesProvider>
         </NextUIProvider>
